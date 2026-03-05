86689536858ef1692b1e5da07dce1ddd24cf1feb
Filippohh <filippopicino@gmail.com>
2026-03-04 23:53:45 -0300

    feat: adiciona métodos de comportamento na classe Mochila

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,22 +1,46 @@
 package br.com.fiapride.main;
 
-//Importamos a classe Passageiro para que o sistema a reconheça
+// Importamos a classe Mochila
 import br.com.fiapride.model.Mochila;
 
 public class SistemaPrincipal {
 
- public static void main(String[] args) {
-	// Dentro do main...// Fabriquei a primeira (Instância 1)
-	 Mochila minhaMochila = new Mochila();
-	 minhaMochila.cor = "Azul";
-	 minhaMochila.pesoMaximoKg = 30;
-	 // Fabriquei a segunda (Instância 2)
-	 Mochila mochilaDoProfessor = new Mochila();
-	 mochilaDoProfessor.cor = "laranja";
-	 mochilaDoProfessor.pesoMaximoKg = 40;
-	 System.out.println("Minha mochila é: " + minhaMochila.cor);
-	 System.out.println("A do professor é: " + mochilaDoProfessor.cor);
-	 System.out.printf("Minha mochila aguenta: %.1f kg%n", minhaMochila.pesoMaximoKg);
-	 System.out.printf("A do professor aguenta: %.1f kg%n", mochilaDoProfessor.pesoMaximoKg);
- }
+    public static void main(String[] args) {
+
+        System.out.println("=== Sistema de Mochilas ===");
+
+        // INSTANCIAÇÃO
+        // Criando o objeto mochila1
+        Mochila mochila1 = new Mochila("Verde", "Enzo");
+
+        System.out.println("\nAdição de peso na mochila 1");
+        mochila1.adicionarPeso(3.5);
+
+        System.out.println("\nRemoção de peso da mochila 1");
+        mochila1.removerPeso(1.0);
+
+        System.out.println("\nTentando remover peso inválido");
+        mochila1.removerPeso(10);
+
+        System.out.println("\nEstado da Mochila 1");
+        System.out.println("Dono: " + mochila1.donoDaMochila);
+        System.out.println("Cor: " + mochila1.cor);
+        System.out.println("Peso atual: " + mochila1.pesoNaMochila + " kg");
+
+
+        // Criando o objeto mochila2
+        Mochila mochila2 = new Mochila("Preta", "Maria");
+
+        System.out.println("\nAdição de peso na mochila 2");
+        mochila2.adicionarPeso(5);
+
+        System.out.println("\nTentando adicionar peso inválido");
+        mochila2.adicionarPeso(-2);
+
+        System.out.println("\nEstado da Mochila 2");
+        System.out.println("Dono: " + mochila2.donoDaMochila);
+        System.out.println("Cor: " + mochila2.cor);
+        System.out.println("Peso atual: " + mochila2.pesoNaMochila + " kg");
+
+    }
 }
--- src/br/com/fiapride/model/Mochila.java
+++ src/br/com/fiapride/model/Mochila.java
@@ -2,6 +2,42 @@ package br.com.fiapride.model;
 public class Mochila { // <-- Use o nome do SEU objeto    
     // As características que você pensou    
     public String cor;
-    public double pesoMaximoKg;
+    public String donoDaMochila;
+    public double pesoNaMochila;
     
+    public Mochila(String cor, String donoDaMochila) {
+    	this.cor = cor;
+    	this.donoDaMochila = donoDaMochila;
+    	this.pesoNaMochila = 0.0;
+    }
+    
+    public void adicionarPeso(double peso) {
+    	
+    	if (peso <= 0) {
+    		System.out.println("Erro: O peso adicional deve ser maior que zero.");
+    		
+    				return;
+    	}
+    	this.pesoNaMochila += peso;
+    	System.out.println("Peso adicionado. Novo saldo: " + this.pesoNaMochila);
+    }
+    
+    public void removerPeso(double pesoRemovido) {
+    	
+    	if (pesoRemovido <= 0) {
+    		System.out.println("Erro: esse peso não pode ser removido.");
+    		return;
+    	}
+    	
+    	if (this.pesoNaMochila < pesoRemovido) {
+    		System.out.println("Erro: não pode ser removido mais peso do que a mochila possui.");
+    		return;
+    	}
+    	
+    	this.pesoNaMochila -= pesoRemovido;
+    	System.out.println("Peso removido. Peso restante: " + this.pesoNaMochila);
+    }
 }
+
+    
+    
